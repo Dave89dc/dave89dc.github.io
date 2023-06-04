1b312d3deaf5361c5fea79db6d2e78cc6958ab4f
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-06-04 21:54:45 +0200

    home, biography, css and JS

---
+++ main.js
@@ -0,0 +1,17 @@
+function displayYears(){
+    const etàList = document.getElementById('età');
+    const text = document.createTextNode(`Età: ${getYears()}`);
+    etàList.appendChild(text);
+}
+
+function getYears(){
+    const date = new Date();
+    const milliSec = date.getTime();
+    const myDate = new Date('1989-09-29')
+    const myMillSec = myDate.getTime();
+    const difference = milliSec - myMillSec;
+    const anni = Math.floor(difference / (1000 * 60 * 60 * 24 * 365.25));
+    return anni;
+}
+
+displayYears();
